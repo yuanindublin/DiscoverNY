7f6898f05834ff80946751337b6740b6ca2f40dd
ZRQ-rikkie <Z986929216@gmail.com>
2023-07-23 20:41:16 +0100

    tags api

--- BackEnd/backend_django/POIs/urls.py
+++ BackEnd/backend_django/POIs/urls.py
@@ -3,8 +3,10 @@ from rest_framework.routers import DefaultRouter
 from .views import POIViewSet
 
 router = DefaultRouter()
-router.register('', POIViewSet)
+router.register('', POIViewSet, basename='poi')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('tag/<str:tag>/', POIViewSet.as_view({'get': 'tag'}), name='poi-tag'),
 ]
+
