5b76198564b3f88031c4970e448a7a9238af06a2
ZRQ-rikkie <Z986929216@gmail.com>
2023-07-23 22:02:11 +0100

    making forcast

--- BackEnd/backend_django/backend_django/settings.py
+++ BackEnd/backend_django/backend_django/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     "bucketlist",
     "POIs",
     "weather"
-
 ]
 
 MIDDLEWARE = [
--- BackEnd/backend_django/forecast/apps.py
+++ BackEnd/backend_django/forecast/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TagsConfig(AppConfig):
+class ForecastConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "tags"
+    name = "forecast"
