206b474d8bf252adb68f26ab111013b527bf4d61
ZRQ-rikkie <Z986929216@gmail.com>
2023-08-08 16:54:04 +0100

    api loading

--- BackEnd/backend_django/POIs/serializers.py
+++ BackEnd/backend_django/POIs/serializers.py
@@ -1,25 +1,27 @@
 from rest_framework import serializers
-from nybusy.models import POI, POIImage, PredictZone
-from prediction.serializers import PredictZoneSerializer
+from nybusy.models import POI, POIImage, PredictPOI  # Make sure to import PredictPOI
 
+class PredictPOISerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PredictPOI
+        fields = ['time', 'busylevel', 'time_index', 'busyindex']
 
 class POIImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = POIImage
         fields = ['id', 'image']
 
-
 class POISerializer(serializers.ModelSerializer):
     coordinate = serializers.SerializerMethodField()
     images = POIImageSerializer(many=True, read_only=True)
-    predictions = serializers.SerializerMethodField()  # Add this line
+    predictions = serializers.SerializerMethodField()
 
     class Meta:
         model = POI
         fields = ['id', 'addr_city', 'addr_housenumber', 'addr_postcode',
                   'addr_street', 'description', 'geometry', 'location_id',
                   'name', 'opening_hours', 'tags', 'website', 'zone',
-                  'coordinate', 'images', 'predictions']  # Add 'predictions' here
+                  'coordinate', 'images', 'predictions']
 
     def get_coordinate(self, obj):
         return {
@@ -28,20 +30,18 @@ class POISerializer(serializers.ModelSerializer):
         }
 
     def get_predictions(self, obj):
-        predictions = PredictZone.objects.filter(location_id=obj.location_id)
+        predictions = PredictPOI.objects.filter(poi=obj)  # Change the filter to match the POI directly
 
         # Get the time query parameter from the request
         time_index_str = self.context['request'].query_params.get('time_index', None)
         if time_index_str is not None:
             time_index = int(time_index_str)
             if time_index is not None:
-                # If a time query parameter is provided, use it to filter the PredictZone query
+                # If a time query parameter is provided, use it to filter the PredictPOI query
                 predictions = predictions.filter(time_index=time_index)
 
         if predictions:
-            serializer = PredictZoneSerializer(predictions, many=True, context=self.context)
+            serializer = PredictPOISerializer(predictions, many=True, context=self.context)
             return serializer.data
 
         return None
-
-
